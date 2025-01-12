d05a84ea8bb0b627ebea67d013578a74690d93ee
euq <hpavlakis@disroot.org>
2025-01-12 19:57:41 +0200

    Feature A: New random string generator function with HEX values & test - benchmark implementation

--- pkg/util/string.go
+++ pkg/util/string.go
@@ -31,3 +31,29 @@ func RandString(n int) string {
 
 	return string(b)
 }
+
+// RandStringHex returns a random string of length n.
+func RandStringHex(n int) string {
+	const hexBytes = "0123456789ABCDEF"
+	const (
+		hexIdxBits = 4                       // 4 bits to represent a hex index
+		hexIdxMask = 1<<hexIdxBits - 1       // Mask to extract 4 bits (0b1111 or 15)
+		hexIdxMax  = hexIdxMask / hexIdxBits // # of hex indices fitting in 63 bits
+	)
+
+	b := make([]byte, n)
+	// A rand.Int63() generates 63 random bits, enough for hexIdxMax hex characters!
+	for i, cache, remain := n-1, randx.Int63(), hexIdxMax; i >= 0; {
+		if remain == 0 {
+			cache, remain = randx.Int63(), hexIdxMax
+		}
+		if idx := int(cache & hexIdxMask); idx < len(hexBytes) {
+			b[i] = hexBytes[idx]
+			i--
+		}
+		cache >>= hexIdxBits
+		remain--
+	}
+
+	return string(b)
+}
